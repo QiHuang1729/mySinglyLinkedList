a18209164f50d7b951829041b2a994e1d3edb70f
QiHuang1729 <130502220+QiHuang1729@users.noreply.github.com>
2024-05-06 10:09:42 -0700

    Add files via upload

--- SinglyLinkedList/SinglyLinkedList.java
+++ SinglyLinkedList/SinglyLinkedList.java
@@ -9,7 +9,6 @@ import java.util.NoSuchElementException;
  */
 public class SinglyLinkedList<E extends Comparable<E>>
 {
-	// E extends Comparable<E>? Interesting
 	/* Fields */
 	private ListNode<E> head, tail;		// head and tail pointers to list
 	
@@ -20,17 +19,26 @@ public class SinglyLinkedList<E extends Comparable<E>>
 	public SinglyLinkedList(SinglyLinkedList<E> oldList) {}
 	
 	/**	Clears the list of elements */
-	public void clear() {}
+	public void clear() {
+		head = null;
+		tail = null;
+	}
 	
 	/**	Add the object to the end of the list
 	 *	@param obj		the object to add
 	 *	@return			true if successful; false otherwise
 	 */
 	public boolean add(E obj) {
+		
 		try {
 			ListNode<E> toAdd = new ListNode<E>(obj);
-			tail.setNext(toAdd);
-			tail = toAdd;
+			if (size() == 0) {
+				head = toAdd;
+				tail = toAdd;
+			} else {
+				tail.setNext(toAdd);
+				tail = toAdd;
+			}
 			return true;
 		} catch (OutOfMemoryError e) {
 			return false;
@@ -46,24 +54,49 @@ public class SinglyLinkedList<E extends Comparable<E>>
 	public boolean add(int index, E obj) {
 		try {
 			ListNode<E> toAdd = new ListNode<E>(obj);
-			ListNode<E> temp = head;
-			if (index == 0) {
-				head = toAdd;
-				toAdd.setNext(temp);
-			}
-			for (int i = 0; i < index; i++) {
-				temp = temp.getNext();
-			}
+			ListNode<E> prev = null;
+			ListNode<E> next = null;
+			ListNode<E> temp = null;
 			
+			if (index == 0) {
+				next = head;
+				head = toAdd;
+				toAdd.setNext(next);
+			} else if (0 < index && index < size()) {
+				temp = head;
+				
+				for (int i = 0; i < index - 1; i++) {
+					temp = temp.getNext();
+				}
+				
+				prev = temp;
+				next = prev.getNext();
+				prev.setNext(toAdd);
+				toAdd.setNext(next);
+			} else if (index == size()) {
+				add(obj);
+			} else {
+				throw new NoSuchElementException();
+			}
 			
 		} catch (OutOfMemoryError e) {
 			return false;
 		}
+		
+		return true;
 	}
 	
 	/**	@return the number of elements in this list */
 	public int size() {
-		return 0;
+		int count = 0;
+		ListNode<E> temp = head;
+		
+		while (temp != null) {
+			temp = temp.getNext();
+			count++;
+		}
+		
+		return count;
 	}
 	
 	/**	Return the ListNode at the specified index
@@ -72,7 +105,17 @@ public class SinglyLinkedList<E extends Comparable<E>>
 	 *	@throws NoSuchElementException if index does not exist
 	 */
 	public ListNode<E> get(int index) {
-		return null;
+		ListNode<E> theNode = head;
+		
+		if (index < 0 || index >= size()) {
+			throw new NoSuchElementException();
+		}
+		
+		for (int i = 0; i < index; i++) {
+			theNode = theNode.getNext();
+		}
+		
+		return theNode;
 	}
 	
 	/**	Replace the object at the specified index
@@ -82,7 +125,38 @@ public class SinglyLinkedList<E extends Comparable<E>>
 	 *	@throws NoSuchElementException if index does not exist
 	 */
 	public E set(int index, E obj) {
-		return null;
+		
+		// note to self: the code is less intuitive without ListNode<E>
+		// next, but it uses less variables.
+		ListNode<E> newElement = new ListNode<>(obj);
+		ListNode<E> temp = head;
+		ListNode<E> prev = null;
+		ListNode<E> oldElement = null;
+		
+		if (index == 0) {
+			temp = temp.getNext();
+			newElement.setNext(temp);
+			oldElement = head;
+			head = newElement;
+		} else if (0 < index && index <= size() - 1) {
+			for (int i = 0; i < index - 1; i++) {
+				temp = temp.getNext();
+			}
+			
+			prev = temp;
+			for (int i = 0; i < 2; i++) {
+				temp = temp.getNext();
+			}
+			
+			newElement.setNext(temp);
+			oldElement = prev.getNext();
+			prev.setNext(newElement);
+		} else if (index == size() - 1) {
+			oldElement = tail;
+			tail = newElement;
+		}
+		
+		return oldElement;
 	}
 	
 	/**	Remove the element at the specified index
@@ -91,12 +165,45 @@ public class SinglyLinkedList<E extends Comparable<E>>
 	 *	@throws NoSuchElementException if index does not exist
 	 */
 	public E remove(int index) {
-		return null;
+		ListNode<E> temp = head;
+		ListNode<E> prev = null;
+		ListNode<E> removed = null;
+		
+		if (index == 0) {
+			removed = temp;
+			temp = temp.getNext();
+			head = temp;
+		} else if (0 < index && index < size()) {
+			for (int i = 0; i < index - 1; i++) {
+				temp = temp.getNext();
+			}
+			
+			prev = temp;
+			
+			if (index == size() - 1) {
+				// the tail is the element being removed
+				removed = tail;
+				tail = prev;
+				prev.setNext(null);
+			} else {
+				// the element being removed 
+				temp = temp.getNext();
+				removed = temp;
+				
+				// the element after the one being removed
+				temp = temp.getNext();
+				prev.setNext(temp);
+			}
+		} else {
+			throw new NoSuchElementException();
+		}
+		
+		return removed.getValue();
 	}
 	
 	/**	@return	true if list is empty; false otherwise */
 	public boolean isEmpty() {
-		return false;
+		return head == null;
 	}
 	
 	/**	Tests whether the list contains the given object
@@ -104,7 +211,18 @@ public class SinglyLinkedList<E extends Comparable<E>>
 	 *	@return				true if the object is in the list; false otherwise
 	 */
 	public boolean contains(E object) {
-		return false;
+		boolean isContained = false;
+		ListNode<E> temp = head;
+		
+		for (int i = 0; i < size(); i++) {
+			E nodeValue = temp.getValue();
+			if (nodeValue.equals(object)) {
+				isContained = true;
+			}
+			temp = temp.getNext();
+		}
+		
+		return isContained;
 	}
 	
 	/**	Return the first index matching the element
@@ -112,7 +230,19 @@ public class SinglyLinkedList<E extends Comparable<E>>
 	 *	@return				if found, the index of the element; otherwise returns -1
 	 */
 	public int indexOf(E element) {
-		return -1;
+		int index = -1;
+		ListNode<E> temp = head;
+		E nodeValue = null;
+		
+		for (int i = 0; i < size(); i++) {
+			nodeValue = temp.getValue();
+			if (nodeValue.equals(element) && index == -1) {
+				index = i;
+			}
+			temp = temp.getNext();
+		}
+		
+		return index;
 	}
 	
 	/**	Prints the list of elements */
